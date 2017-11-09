38d3e5d81e63d6cc7fb04710eb305e326c2d62b8
Sam Rosen <srosen@udel.edu>
2017-11-09 13:48:00 -0500

    templates added

--- songbase.py
+++ songbase.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return '<h1>hello world!!!!! lalalala</h1>'
+    # return '<h1>hello world!!!!! lalalala</h1>'
+    return render_template('index.html')
+
+
+@app.route('/songs')
+def get_all_songs():
+    songs = [
+     'Look at Me Now' ,
+     'Forever' ,
+     'Ayo' 
+    ]
+    return render_template('songs.html', songs=songs)
 
 
 @app.route('/user/<string:name>/')
 def get_user(name):
-    return '<h1>hello %s your age is %d</h1>' % (name, 3)
+    # return '<h1>hello %s your age is %d</h1>' % (name, 3)
+    return render_template('user.html', user_name=name)
 
 
 if __name__ == '__main__':
